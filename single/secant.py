d1d2a71c544cdc8dcdb97359a8320819b27d65d3
Даниил <deadxraver@yandex.ru>
2025-03-25 00:27:09 +0300

    метод секущих

---
+++ single/secant.py
@@ -0,0 +1,11 @@
+def find_solution(x0, f, eps):
+	x_prev_prev = x0
+	x_prev = x_prev_prev + eps
+	x = 0
+	for i in range(1000):
+		x = x_prev - f(x_prev) * (x_prev - x_prev_prev) / (f(x_prev) - f(x_prev_prev))
+		if abs(x_prev - x) <= eps or abs(f(x)) <= eps:
+			return {"found": True, "x": x, "iters": i}
+		x_prev_prev = x_prev
+		x_prev = x
+	return {"found": False, "x": x, "iters": 1000, "msg": "Слишком много итераций, вероятно не сходится"}
